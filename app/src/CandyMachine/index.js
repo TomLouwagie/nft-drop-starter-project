3716cd8fcfb3280436895d16bee0aedf0778a702
Tom Louwagie <tom@louwagie.com>
2022-01-16 01:29:21 +0100

    adding countdown timer

--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './CountdownTimer.css';
+
 import CountdownTimer from '../CountdownTimer';
 
 import { Connection, PublicKey } from '@solana/web3.js';
@@ -411,18 +411,6 @@ const CandyMachine = ({ walletAddress }) => {
 
 };
 
-const CountdownTimer = ({ dropDate }) => {
-  // State
-  const [timerString, setTimerString] = useState('');
-
-  return (
-    <div className="timer-container">
-      <p className="timer-header">Candy Drop Starting In</p>
-      {timerString && <p className="timer-value">{`⏰ ${timerString}`}</p>}
-    </div>
-  );
-};
-
 // Create render function
 const renderDropTimer = () => {
   // Get the current date and dropDate in a JavaScript Date object
@@ -439,43 +427,6 @@ const renderDropTimer = () => {
   // Else let's just return the current drop date
   return <p>{`Drop Date: ${candyMachine.state.goLiveDateTimeString}`}</p>;
 };
-
-
-// Our useEffect will run on component load
-useEffect(() => {
-  console.log('Setting interval...');
-
-  // Use setInterval to run this piece of code every second
-  const interval = setInterval(() => {
-    const currentDate = new Date().getTime();
-    const distance = dropDate - currentDate;
-
-    // Here it's as easy as doing some time math to get the different properties
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // We have our desired output, set it in state!
-    setTimerString(`${days}d ${hours}h ${minutes}m ${seconds}s`);
-
-    // If our distance passes zero this means that it's drop time!
-    if (distance < 0) {
-      console.log('Clearing interval...');
-      clearInterval(interval);
-    }
-  }, 1000);
-
-  // Anytime our component unmounts let's clean up our interval
-  return () => {
-    if (interval) {
-      clearInterval(interval);
-    }
-  };
-}, []);
-
 
 return (
   candyMachine.state && (
@@ -502,4 +453,3 @@ return (
 );
 
 export default CandyMachine;
-export default CountdownTimer;
